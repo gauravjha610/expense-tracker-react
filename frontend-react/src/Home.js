4f67f085e6e6d9d797b04d4bd438953cb7e718ec
GauravJha610 <gaurav.jha.cse24@heritageit.edu.in>
2026-05-09 02:45:35 +0530

    added income,expenses,totalBalance,showTransactions

--- frontend-react/src/Home.js
+++ frontend-react/src/Home.js
@@ -1,23 +1,85 @@
 import React from 'react'
 import './Home.css'
 
+import { useState } from 'react';
+
 function Home() {
+
+  const [inputNum, setInputNum] = useState("");
+  const [inputNum2, setInputNum2] = useState("");
+  const [netBalance, setNetBalance] = useState(0);
+
+  const [showTransaction,setShowTransaction]= useState(false);
+
+
+  const [transaction,setTransaction] = useState([]);
+
+  const addTransaction=(type,amount)=>{
+    const newTransaction={
+      type,
+      amount
+    };
+    setTransaction((prev)=>[...prev, newTransaction]);
+  }
+
+  const addAmount = ()=>{
+    setNetBalance(netBalance + Number(inputNum));
+    setInputNum("");
+  }
+
+  const subtractAmount = () => {
+    setNetBalance(netBalance - Number(inputNum2));
+    setInputNum2("");
+  };
+
   return (
     <div className='Homepage'>
 
-        <div className='showinfo'>
-            <p>Total net balance:</p>
-            <button>Show transactions</button>
+        <div className='balanceinfo'>
+          <h2>Total net balance: <i class="fa-solid fa-indian-rupee-sign"></i> {netBalance}</h2>
         </div>
 
         <div className='editinfo'>
-            <div className='addexpense'>
+          <div className='box'>
+            <h2 className='title'>Income <i class="fa-solid fa-arrow-trend-up"></i></h2>
+            <input className='inputbox'
+            type="number"
+            step="any"
+            value={inputNum}
+            onChange={(e) => setInputNum(e.target.value)}
+            placeholder="Enter amount"
+            />
+            <button className='buttonIncome' onClick={()=>{addAmount();addTransaction("Income",inputNum)}}>Add Income</button>
+          </div>
 
-            </div>
-            <div className='addincome'>
-
-            </div>
+          <div className='box'>
+            <h2 className='title'>Expenses <i class="fa-solid fa-arrow-trend-down"></i></h2>
+            <input className='inputbox'
+            type="number"
+            step="any"
+            value={inputNum2}
+            onChange={(e) => setInputNum2(e.target.value)}
+            placeholder="Enter amount"
+            />
+            <button className='buttonExpense' onClick={()=>{subtractAmount();addTransaction("Expense",inputNum2)}}>Add Expense</button>
+          </div>
         </div>
+
+        <div className='transactionBox'>
+          <button className='buttonTransaction' onClick={()=>{setShowTransaction(!showTransaction)}}>{showTransaction?"Hide":"Show"} Transactions</button>
+
+          {showTransaction && (
+            <div className='transactionList'>
+            {[...transaction].reverse().map((item, index) => (
+              <div key={index} className={`transactionElement ${item.type}`}>
+                <h2>{item.type}</h2>
+                <h2>₹{item.amount}</h2>
+              </div>
+              ))}
+          </div>
+          )}
+        </div>
+
     </div>
   )
 }
--- frontend-react/src/Nav.js
+++ frontend-react/src/Nav.js
@@ -4,7 +4,7 @@ import './Nav.css'
 function Nav() {
   return (
     <div className='navbar'>
-        <h1>Expense Tracker App</h1>
+        <h1>Expense Tracker App <i class="fa-solid fa-wallet"></i></h1>
         <h4>--A simple solution to track your expenses--</h4>
     </div>
   )
